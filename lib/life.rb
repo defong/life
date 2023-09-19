6582a7bde9f371ddcb64082d9351b7666674d144
defong <de_fong2k@yahoo.com>
2023-09-19 15:58:42 +0100

    Added init setup

---
+++ lib/life.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "life/version"
+
+module Life
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/life_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Life do
+  it "has a version number" do
+    expect(Life::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
